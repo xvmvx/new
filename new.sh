84a2c109cf31f0685260b0595fee0ad0637ce21a
xvmvx <107462385+xvmvx@users.noreply.github.com>
2023-01-08 23:30:30 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -89,6 +89,11 @@ if [[ ${reME} = "y" ]]; then
             green "增加root用户。。。完成✅✅✅！"
         else
             red "添加用户未成功！"
+            red "手动添加请使用  adduser 添加的用户名"
+            yellow " 修改 /etc/sudoers 文件，在root ALL=(ALL) ALL下面添加一行，如下所示："
+            red "用户名 ALL=(ALL) ALL "
+            yellow " 然后修改用户，使其属于root组（wheel），命令如下："
+            red "usermod -g root 用户名"
         fi
     fi
 elif [[ ${reME} = "0" ]]; then
