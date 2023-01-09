fb841f5cd76b0ade1c565d610d71cffd3428b81e
xvmvx <107462385+xvmvx@users.noreply.github.com>
2023-01-09 19:46:57 +0800

    Create run.sh

---
+++ iptable/run.sh
@@ -0,0 +1,9 @@
+
+iptables.sh
+示例文件路径：/root/shell/iptables.sh
+脚本运行方式：第一次运行，每次更新脚本内容后手动运行
+脚本运行命令：/root/shell/iptables.sh
+
+示例脚本路径：/root/shell/BlackIP.sh
+脚本运行方式：添加至 crontab 定期运行更新
+脚本运行命令：/bin/nohup /root/shell/BlackIP.sh > /tmp/IPSet_Shell_Cron.log 2>&1 &
