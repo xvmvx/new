280790eca41e59f7aaa4fcfc4430f66ccac5eb14
xvmvx <107462385+xvmvx@users.noreply.github.com>
2023-01-11 12:26:49 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,14 +12,14 @@ blue(){
     echo -e "\033[34m\033[01m$1\033[0m"
 }
 red "############  Docker  #################"
-yellow " Docker  基本安装"
-yellow " Docker- compose 安装"
-yellow " Docker IP6开通"
-yellow " Docker 更换国内源"
-yellow " Fast OS Docker面板"
-yellow " NPM 安装"
-yellow " Portainer 安装"
-red "安装Docker(1),Docker- compose(2),设置(3),NPM（4),Portainer（5）"
+yellow "1. Docker  基本安装"
+yellow "2. Docker- compose 安装"
+yellow "3. Docker 一键脚本"
+yellow "4。Docker 一键脚本2"
+yellow "5。Docker IP6开通"
+yellow "6. Docker 更换国内源"
+yellow "7. Fast OS Docker面板"
+yellow "8. Portainer 安装面板"
 read -p "："  ddd
 if [[ "$ddd" = "1" ]]; then
     yellow "安装Docker："
@@ -36,47 +36,30 @@ if [[ "$ddd" = "1" ]]; then
      if [ $? = '0' ]; then
         green "Docker...安装完成✅✅✅！"
      else
-        red "安装失败，可输入数字尝试一键安装！"
-        echo -n "输入要使用的安装命令：      > "
+        red "安装失败，是否尝试一键安装（Y)！"
         read character
-        case $character in
-            1 ) 
-                source docker1.sh
-            ;;
-            2 ) 
-                curl -fsSL https://get.docker.com | bash -s docker
-            ;;
-            3 ) 
-                source docker2.sh
-            ;;
-            * ) echo 输入不符合要求
-            esac          
+        if [ "$character" = "y" ]: then
+            curl -fsSL https://get.docker.com | bash -s docker
         fi
+    fi
+    
 # docker-dompose
 elif [[ "$ddd" = "2" ]]; then
-    sudo apt install docker-compose  
+    sudo apt install docker-compose || bash <(curl -sSL https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.sh)
     if [ $? = '0' ]; then
          green "【docker-compose】...安装完成✅✅✅！"
     else
-        red "安装失败，可输入数字尝试一键安装！"
-        echo -n "输入要使用的安装命令：      > "
+        red "安装失败，是否尝试一键安装（Y)！"
         read character
-        case $character in
-            1 ) 
-                bash <(curl -sSL https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.sh)
-            ;;
-            2 ) 
-                curl -L https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-linux-`uname -m` > ./docker-compose
-            ;;
-            3 ) 
-                source docker1.sh
-            ;;
-            * ) echo 输入不符合要求
-            esac          
+        if [ "$character" = "y" ]: then
+            curl -L https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-linux-`uname -m` > ./docker-compose      
         fi
-
-# docker优化
+    fi
 elif [[ "$ddd" = "3" ]]; then
+    sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/docker1.sh -O -)"
+elif [[ "$ddd" = "4" ]]; then
+    sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/docker2.sh -O -)"
+elif [[ "$ddd" = "5" ]]; then
 cat > /etc/docker/daemon.json <<EOF
 {
     "log-driver": "json-file",
@@ -99,7 +82,7 @@ sudo systemctl enable docker
         exit 1
     fi
 # npm
-elif [[ "$ddd" = "4" ]]; then
+elif [[ "$ddd" = "6" ]]; then
     wget -N --no-check-certificate https://raw.githubusercontent.com/xvmvx/new/main/do/docker_yuan.sh && chmod +x docker_yuan.sh && bash docker_yuan.sh
      if [ $? = '0' ]; then
         green "已完成国内源更换✅✅✅！"
@@ -107,9 +90,9 @@ elif [[ "$ddd" = "4" ]]; then
         red "更换失败，人工检查！"
         exit 1
     fi
-elif [[ "$ddd" = "5" ]]; then
+elif [[ "$ddd" = "7" ]]; then
     wget -N --no-check-certificate  https://raw.githubusercontent.com/xvmvx/new/main/do/fastos.sh && chmod +x fastos.sh  && bash fastos.sh
-elif [[ "$ddd" = "6" ]]; then
+elif [[ "$ddd" = "8" ]]; then
     wget -N --no-check-certificate  https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh && chmod +x portainer.sh  && bash portainer.sh
 fi
 
