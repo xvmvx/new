9c9a1bd09385ab994ce298e43866d6bc2e1e5937
xvmvx <107462385+xvmvx@users.noreply.github.com>
2023-01-11 10:18:25 +0800

    Update .sh

--- .sh
+++ .sh
@@ -38,44 +38,141 @@ myIP2=$(ip route get 1 | awk '{print $7;exit}')
 yellow "${logo1}"
 ## [new](https://github.com/xvmvx/new)是新安装系统的维护
 red "准备好了吗？"
-blue "我们有 （0)系列------基础类"
-green "01   更改端口的小脚本"
-green "02   alias缩短命令使用"
-green "03   宝塔面板的相关操作"
-green "04   nginx反向代理管理"
-green "05   Docker相关的操作"
-green "06   Fast OS面板的安装"
-green "07   Docker可视面板安装"
-blue "我们有 （1)系列------web类"
-green "11   dashy面板"
-green "12   Hexo 面板"
-green "13   Ward面板"
-green "14   ServerStatus面板"
-green "15   Uptime Kuma面板"
-green "16   Guacamole远程RAC&SSH"
-green "17   VPN & noVPN"
-green "18   cryptgeon"
-green "19   FRP+NPM+VP"
+blue "          0)系列------基础类"
+green "比如更改端口/时区/端口的小脚本等等"
+blue "          1)系列------web类"
+green "比如监视/图形化服务器的各类面板等"
+blue "          2)系列------网盘类"
+green "比如离线下载ari2/nextcloud云等"
+blue "          3)系列------目录类"
+green "比如图床在线笔记远程桌面等"
+yellow "输入对应数字浏览相关细节，返回 00 "
+read -p ":   " inCHANGE
+if [ ${#inCHANGE} = "1" ]; then
+    case "$inCHANGE" in
+        0)
+            green "01   更改端口的小脚本"
+            green "02   alias缩短命令使用"
+            green "03   宝塔面板的相关操作"
+            green "04   nginx反向代理管理"
+            green "05   Docker相关的操作"
+            green "06   Fast OS面板的安装"
+            green "07   Docker可视面板安装"
+        ;;
+        1)
+            green "11   dashy面板"
+            green "12   Hexo 面板"
+            green "13   Ward面板"
+            green "14   ServerStatus面板"
+            green "15   Uptime Kuma面板"
+            green "16   Guacamole远程RAC&SSH"
+            green "17   VPN & noVPN"
+            green "18   cryptgeon"
+            green "19   FRP+NPM+VP"
+        ;;
+        2)
+            green "21   aria2"
+            green "22   nextcloud"
+            green "23   Cloudreve"
+            green "24   FileRun"
+            green "25   Wiki.js"
+            green "26   Trilium"
+            green "27   Joplin"
+            green "28   为知笔记"
+            green "29   Rocket"
+        ;;
+        3)
+            green "31   楼兰图床"
+            green "32   AllTube"
+            green "33   Fireshare"
+            green "34   platform"
+            green "35   searxng"
+            green "36   Pingvin"
+            green "37   Syncthing"
+            green "38   ZFile"
+            green "39   Memos"
+        ;;
+        *)
+            clear
+            red "请输入正确数字 !"
+            start_menu
+        ;;
+    esac
+elif [ ${#inCHANGE}  = "2" ]; then
+    a$inCHANGE
+elif [ ${#inCHANGE}  = "0" ]; then
+    red "请输入正确数字 !"
+fi
+#01 更改端口的小脚本
+function a01(){
+# wget -O "/root/unixbench.sh" "https://raw.githubusercontent.com/BlueSkyXN/ChangeSource/master/unixbench.sh" --no-check-certificate -T 30 -t 5 -d
+# curl -o- -L https://raw.githubusercontent.com/xvmvx/new/main/ssh22.sh | bash -s -- --version 0.16.1
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/ssh22.sh -O -)"
+}
+#02   alias缩短命令使用
+function a02(){
+wget https://raw.githubusercontent.com/xvmvx/new/main/my.bashrc
+mv /my.bashrc ~/.bashrc
+source ~/.bashrc
+}
+#03   宝塔面板的相关操作
+function a03(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/baota.sh -O -)"
+}
+#04   nginx反向代理管理
+function a04(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/npm.sh -O -)"
+}
+#05   Docker相关的操作
+function a05(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/docker.sh-O -)"
+}
+#06   Fast OS面板的安装
+function a06(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/fastos.sh -O -)"
+}
+#07   Docker可视面板安装"
+function a07(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh -O -)"
+}
+#11   dashy面板
+function a11(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/dashy/dashy.sh -O -)"
+}
+#12   Hexo 面板  ???????????
+function a12(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh -O -)"
+}
+#13   Ward面板
+function a13(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/ward.sh -O -)"
+}
+#14   ServerStatus面板
+function a07(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh -O -)"
+}
+#15   Uptime Kuma面板
+function a07(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh -O -)"
+}
+#16   Guacamole远程RAC&SSH
+function a07(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh -O -)"
+}
+#17   VPN & noVPN
+function a07(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh -O -)"
+}
+#18   cryptgeon
+function a07(){
+sh -c "$(wget https://raw.githubusercontent.com/xvmvx/new/main/do/portainer.sh -O -)"
+}
+#19   FRP+NPM+VP"
 blue "我们有 （2)系列------网盘类"
-green "21   aria2"
-green "22   nextcloud"
-green "23   Cloudreve"
-green "24   FileRun"
-green "25   Wiki.js"
-green "26   Trilium"
-green "27   Joplin"
-green "28   为知笔记"
-green "29   Rocket"
+
 blue "我们有 （3)系列------目录类"
-green "31   楼兰图床"
-green "32   AllTube"
-green "33   Fireshare"
-green "34   platform"
-green "35   searxng"
-green "36   Pingvin"
-green "37   Syncthing"
-green "38   ZFile"
-green "39   Memos"
+
+
 * [ssh-22](https://github.com/xvmvx/new/ssh22.sh)            ----------更改端口的小脚本
 * [bashrc](https://github.com/xvmvx/new/my.bashrc)             ----------alias缩短命令使用
 * [Nginxp](http://165.22.62.26:81)                 ----------反向代理神器_________________[安装]()  [详见](https://blog.laoda.de/archives/nginxproxymanager)
